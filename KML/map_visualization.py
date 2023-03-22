0c80f3ed521f11e8ef2f984c7d9558453f9bfbb8
Karl Gran Grodås <karlgrodaas@gmail.com>
2023-03-22 15:17:42 +0100

    Funker. Mangler Byggteknisk

--- KML/map_visualization.py
+++ KML/map_visualization.py
@@ -1,42 +1,163 @@
 import simplekml
+import random
 import json
 
-with open("KML/files/building_names.txt", 'r') as b:
-    buildings = json.load(b)
+bygg_instabart = [
+    'IPD',
+    'Handleshøyskolen',
+    'Berg',
+    'Byggtkniks',
+    'Elektro A',
+    'Elektro B',
+    'Elektro D+B2',
+    'Eeltro E/F',
+    'Gamle elektro',
+    'Gamle fysikk',
+    'Gamle kjemi',
+    'Grønnbygget',
+    'NTNU. Hovedbygningen',
+    'IT-bygget',
+    'IT-bygget, sydfløy',
+    'Kjelhuset',
+    'Kjemi 1',
+    'Kjemi 2',
+    'Kjemi 3',
+    'Kjemi 4',
+    'Kjemi 5',
+    'Kjemihallen',
+    'Metallurgi',
+    'Oppredning/gruvedrift',
+    'PFI',
+    'Realfagsbygget',
+    'Sentralbygg 1',
+    'Sentralbygg 2',
+    'Statsarkivet',
+    'Tapirbygget',
+    'VM-paviljongne',
+    'Verkstedtekniks'
+    ]
+bygg_geo_engelsk = [
+    'NTNU. Main Administration Building',
+    'Fluids Engineering Building',
+    'P-15',
+    'Product Design Building',
+    'Metallurgy Building',
+    'Berg',
+    'Ore Processing/Mining',
+    'World Championship Pavilion',
+    'Geology Building',
+    'PFI Building',
+    'Materials Engineering Laboratory',
+    'Perleporten',
+    'Materials Technology Building',
+    'Building Technology',
+    'Chemistry Block 1',
+    'Chemistry Block 2',
+    'Chemistry South Wing',
+    'Chemistry Block 3',
+    'Chemistry Block 4',
+    'Chemistry Block 5',
+    'Chemistry Hall',
+    'Central Building 1, south wing',
+    'Central Building 1, tower',
+    'Central Building 1, center wing',
+    'Central Building 2, tower',
+    'Central Building 2, north wing',
+    'Old Chemistry Building',
+    'IT Building, south wing',
+    'IT Building',
+    'Old Physics Building',
+    'Electrical Engineering D+B2',
+    'Old Electrical Engineering Building',
+    'Water Power Laboratory'
+]
+bygg_geo_norsk = [
+    'NTNU. Hovedbygningen',
+    'Strømningstekniske laboratorier',
+    'P-15',
+    'Fraggelberget',
+    'Metallurgi',
+    'Berg',
+    'Oppredning/gruvedrift',
+    'Geologen',
+    'Papirindustriens forskningsinstitutt',
+    'Verkstedtekniske laboratorier',
+    'Perleporten',
+    'Materialtekniske laboratorier',
+    'Kjemiblokk 1',
+    'Kjemiblokk 2',
+    'Kjemi sydfløy',
+    'Kjemiblokk 3',
+    'Kjemiblokk 4',
+    'Kjemiblokk 5',
+    'Kjemihallen',
+    'Sentralbygg 1, søndre lavblokk',
+    'Sentralbygg 1, høyblokk',
+    'Sentralbygg 1, midtre lavblokk',
+    'Sentralbygg 2, høyblokk',
+    'Sentralbygg 2, nordre lavblokk',
+    'Sentralbygg 2, nordre lavblokk',
+    'Gamle kjemi',
+    'IT-bygget, sydfløy',
+    'IT-bygget',
+    'Gamle fysikk',
+    'Gamle elektro',
+    'Elektro D+B2',
+    'Central Building 2, tower'
+]    
+bygg_geo_id = [
+    'relation/13915142',    # Realfagsbygget
+    'way/1039396048',       # Elektro F/E
+    'way/1039395939',       # Elektro B
+    'way/1039395938',       # Varmeteksnik / Kjel
+    'way/1039395917',       # Byggteknisk
+    'relation/184384'
+]
 
-# with open("KML/files/export.geojson", 'r') as f:
-#     data = json.load(f)
-# kml = simplekml.Kml()
-# count = 0
-# for feature in data['features']:
-#     geom = feature['geometry']
-#     geom_type = geom['type']
+# RGB HEX
+intensity = 'FF'
+R = 'FF'
+G = 'FF'
+B = '00'
+
+with open("KML/files/export.geojson", 'r') as f:
+    data = json.load(f)
+kml = simplekml.Kml()
+
+# Iterate throug the geojson file
+for feature in data['features']:
+    geom = feature['geometry']
+    geom_type = geom['type']
     
-#     prop = feature['properties']
-#     try: 
-#         name = prop['name']
-#     except:
-#         name = ''
+    # Get name of buildings
+    prop = feature['properties']
+    try: 
+        try:
+            name = prop['name:en']
+        except:
+            name = prop['name']
+    except:
+        name = ''
+
+    if name in bygg_geo_engelsk:
+        if geom_type == 'Polygon' :
+            test = kml.newpolygon(name=name,
+                        outerboundaryis=geom['coordinates'][0])
+            # test.style.polystyle.color = intensity + B + G + R 
+            test.style.polystyle.colormode = 'random'
+
+    # Execptions for buildings without name
+    prop = feature['properties']
+    try:
+        _id = prop['@id']
+    except:
+        _id = ''
     
-#     bygg = ['IPD' 'NTNU. Hovedbygningen', 'Abels hus']    
-#     if name in bygg:
-#         if geom_type == 'Polygon':
-#             test = kml.newpolygon(name=name,
-#                         outerboundaryis=geom['coordinates'][0])
-#             test.style.polystyle.color = simplekml.Color.changealphaint(100, simplekml.Color.red)
-
+    if _id in bygg_geo_id:
+        if geom_type == 'Polygon':
+            test = kml.newpolygon(name=name,
+                        outerboundaryis=geom['coordinates'][0])
+            #test.style.polystyle.color = intensity + B + G + R 
+            test.style.polystyle.colormode = 'random'
     
-#     # elif geom_type == 'LineString':
-#     #     kml.newlinestring(name='test',
-#     #                       description='test',
-#     #                       coords=geom['coordinates'])
-        
-#     # elif geom_type == 'Point':
-#     #     kml.newpoint(name='test',
-#     #                  description='test',
-#     #                  coords=[geom['coordinates']])
-#     # else:
-#     #     print("ERROR: unknown type:", geom_type)
-
-
-# kml.save('KML/output/kml_file.kml')
+kml.save('KML/output/kml_file.kml')
